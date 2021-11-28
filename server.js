3d8e8dd0ad7c2dc6ef67cc83844f7e1a0832b76c
Manabili <78428017+Manabili@users.noreply.github.com>
2021-11-28 09:58:36 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+
+dotenv.config({path : './config.env'});
+require('./db/conn');
+
+
+app.use(express.json());
+
+app.use(require('./router/auth'));
+
+const PORT = process.env.PORT;
+
+
+//Middleware
+const middleware = (req, res, next) => {
+     
+     next();
+}
+
+
+app.listen(PORT, () => {
+  // perform a database connection when server starts
+    console.log(`Server is running on port: ${PORT}`);
+});
+
